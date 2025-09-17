ab27c316eaa939d71e04718aa87433697fee5199
williams-derek-j <williams-derek-j@proton.me>
2025-09-16 23:25:15 -0700

    ability to instantly find the starting square among the adjacency list
    within the chessboard object is a search object that uses square coordinates as property names, with values being the index in the adjacency list

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -1,8 +1,9 @@
 import Square from "./Square.js";
 
 export default class Chessboard {
-    constructor(length) {
+    constructor(length) { // length of one side of grid, e.g., length for lengthxlength, 12 for 12x12
         this.data = [] // adjacency list
+        this.search = {}
 
         for (let row = 0; row < length; row++) {
             for (let column = 0; column < length; column++) {
@@ -10,32 +11,34 @@ export default class Chessboard {
 
                 const data = []
 
-                if(row + 1 > 0 && row + 1 < 8 && column + 2 > 0 && column + 2 < 8) { // NE
+                if(row + 1 > 0 && row + 1 < length && column + 2 > 0 && column + 2 < length) { // NE
                     data.push([row + 1, column + 2])
                 }
-                if(row + 2 > 0 && row + 2 < 8 && column + 1 > 0 && column + 1 < 8) { // NE
+                if(row + 2 > 0 && row + 2 < length && column + 1 > 0 && column + 1 < length) { // NE
                     data.push([row + 2, column + 1])
                 }
-                if(row - 1 > 0 && row - 1 < 8 && column - 2 > 0 && column - 2 < 8) { // SW
+                if(row - 1 > 0 && row - 1 < length && column - 2 > 0 && column - 2 < length) { // SW
                     data.push([row - 1, column - 2])
                 }
-                if(row - 2 > 0 && row - 2 < 8 && column - 1 > 0 && column - 1 < 8) { // SW
+                if(row - 2 > 0 && row - 2 < length && column - 1 > 0 && column - 1 < length) { // SW
                     data.push([row - 2, column - 1])
                 }
-                if(row + 1 > 0 && row + 1 < 8 && column - 2 > 0 && column - 2 < 8) { // SE
+                if(row + 1 > 0 && row + 1 < length && column - 2 > 0 && column - 2 < length) { // SE
                     data.push([row + 1, column - 2])
                 }
-                if(row + 2 > 0 && row + 2 < 8 && column - 1 > 0 && column - 1 < 8) { // SE
+                if(row + 2 > 0 && row + 2 < length && column - 1 > 0 && column - 1 < length) { // SE
                     data.push([row + 2, column - 1])
                 }
-                if(row - 1 > 0 && row - 1 < 8 && column + 2 > 0 && column + 2 < 8) { // NW
+                if(row - 1 > 0 && row - 1 < length && column + 2 > 0 && column + 2 < length) { // NW
                     data.push([row - 1, column + 2])
                 }
-                if(row - 2 > 0 && row - 2 < 8 && column + 1 > 0 && column + 1 < 8) { // NW
+                if(row - 2 > 0 && row - 2 < length && column + 1 > 0 && column + 1 < length) { // NW
                     data.push([row - 2, column + 1])
                 }
 
                 this.data.push(new Square(coords, data))
+
+                this.search[`${row},${column}`] = this.data.length - 1
             }
         }
     }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import Chessboard from './Chessboard'
 
 const test = new Chessboard(8)
 
-console.log(test.data)
+console.log('adjacency list', test.data)
+console.log('search', test.search)
+console.log('search 0,0', test.search['0,0'])
+console.log('search 7,7', test.search['7,7'])
