52953e3949cba248d009d02d14d55b70d432587f
williams-derek-j <williams-derek-j@proton.me>
2025-09-17 14:43:30 -0700

    BFS working to find target node but not sure how to return the path yet

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -11,28 +11,28 @@ export default class Chessboard {
 
                 const data = []
 
-                if(row + 1 > 0 && row + 1 < length && column + 2 > 0 && column + 2 < length) { // NE
+                if(row + 1 >= 0 && row + 1 < length && column + 2 >= 0 && column + 2 < length) { // NE
                     data.push([row + 1, column + 2])
                 }
-                if(row + 2 > 0 && row + 2 < length && column + 1 > 0 && column + 1 < length) { // NE
+                if(row + 2 >= 0 && row + 2 < length && column + 1 >= 0 && column + 1 < length) { // NE
                     data.push([row + 2, column + 1])
                 }
-                if(row - 1 > 0 && row - 1 < length && column - 2 > 0 && column - 2 < length) { // SW
+                if(row - 1 >= 0 && row - 1 < length && column - 2 >= 0 && column - 2 < length) { // SW
                     data.push([row - 1, column - 2])
                 }
-                if(row - 2 > 0 && row - 2 < length && column - 1 > 0 && column - 1 < length) { // SW
+                if(row - 2 >= 0 && row - 2 < length && column - 1 >= 0 && column - 1 < length) { // SW
                     data.push([row - 2, column - 1])
                 }
-                if(row + 1 > 0 && row + 1 < length && column - 2 > 0 && column - 2 < length) { // SE
+                if(row + 1 >= 0 && row + 1 < length && column - 2 >= 0 && column - 2 < length) { // SE
                     data.push([row + 1, column - 2])
                 }
-                if(row + 2 > 0 && row + 2 < length && column - 1 > 0 && column - 1 < length) { // SE
+                if(row + 2 >= 0 && row + 2 < length && column - 1 >= 0 && column - 1 < length) { // SE
                     data.push([row + 2, column - 1])
                 }
-                if(row - 1 > 0 && row - 1 < length && column + 2 > 0 && column + 2 < length) { // NW
+                if(row - 1 >= 0 && row - 1 < length && column + 2 >= 0 && column + 2 < length) { // NW
                     data.push([row - 1, column + 2])
                 }
-                if(row - 2 > 0 && row - 2 < length && column + 1 > 0 && column + 1 < length) { // NW
+                if(row - 2 >= 0 && row - 2 < length && column + 1 >= 0 && column + 1 < length) { // NW
                     data.push([row - 2, column + 1])
                 }
 
@@ -41,5 +41,47 @@ export default class Chessboard {
                 this.search[`${row},${column}`] = this.data.length - 1
             }
         }
+
+        this.data.forEach((square) => {  // replace coordinates of adjacent nodes with object references to those nodes
+            for (let i = 0; i < square.data.length; i++) {
+                square.data[i] = this.data[this.search[`${square.data[i][0]},${square.data[i][1]}`]]
+            }
+        })
+    }
+
+    travel(start, end, queue = []) { // start and end must be arrays
+        if (queue.length === 0) {
+            if (typeof start !== 'object' && start.length !== 2 && typeof end !== 'object' && end.length !== 2) {
+                return new Error('Bad input!')
+            } else {
+                start = this.data[this.search[`${start[0]},${start[1]}`]]
+                end = this.data[this.search[`${end[0]},${end[1]}`]]
+            }
+
+            // let path = `[${start.x},${start.y}], `
+            queue.push(start)
+        }
+
+        const nextQ = []
+        for (let i = 0; i < queue.length; i++) {{
+            if (queue[i].visited === false) {
+                console.log(queue[i])
+
+                queue[i].visited = true
+
+                if (end === queue[i]) {
+                    // path += `[${adjacent.x},${adjacent.y}]`
+                    return
+                } else {
+                    queue[i].data.forEach((subAdj) => {
+                        if (subAdj.visited === false) {
+                            nextQ.push(subAdj)
+                        }
+                    })
+                }
+            }
+        }}
+
+        return this.travel(start, end, nextQ)
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ console.log('adjacency list', test.data)
 console.log('search', test.search)
 console.log('search 0,0', test.search['0,0'])
 console.log('search 7,7', test.search['7,7'])
+
+test.travel([0,0],[2,1])
